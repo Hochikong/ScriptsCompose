d8247396c266502a1ab5b3f5feaabbbe05acb370
Hochikong <1097225749@qq.com>
2021-11-13 15:37:25 +0800

    init
    Signed-off-by: Hochikong <ckhoidea@hotmail.com>

---
+++ src/main/kotlin/me/ckho/scriptscompose/MainApp.kt
@@ -0,0 +1,12 @@
+package me.ckho.scriptscompose
+
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MainApp
+
+fun main(args: Array<String>) {
+    SpringApplication.run(MainApp::class.java, *args)
+}
