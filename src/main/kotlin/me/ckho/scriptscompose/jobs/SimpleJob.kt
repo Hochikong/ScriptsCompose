1494ae5eb5176711a08cee1ac43eeaa5bdfb29b5
Hochikong <ckhoidea@hotmail.com>
2021-11-28 15:42:07 +0800

    cancellable job
    Signed-off-by: Hochikong <ckhoidea@hotmail.com>

--- src/main/kotlin/me/ckho/scriptscompose/jobs/SimpleJob.kt
+++ src/main/kotlin/me/ckho/scriptscompose/jobs/SimpleJob.kt
@@ -2,27 +2,33 @@ package me.ckho.scriptscompose.jobs
 
 import me.ckho.scriptscompose.domain.dataclasses.ScriptArgSequence
 import me.ckho.scriptscompose.service.impl.ScriptExecutorService
-import org.quartz.Job
+import org.quartz.InterruptableJob
 import org.quartz.JobExecutionContext
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class SimpleJob(
-    @Autowired val executor: ScriptExecutorService
-) : Job{
+//    @Autowired val executor: ScriptExecutorService
+) : InterruptableJob{
+    val executor = ScriptExecutorService()
     private var logger: Logger = LoggerFactory.getLogger(SimpleJob::class.java)
 
     override fun execute(context: JobExecutionContext) {
+        executor.start()
+        logger.info(executor.uid)
         val jdm = context.jobDetail.jobDataMap
         val script: List<ScriptArgSequence> = jdm["scripts"] as List<ScriptArgSequence>
         val wd = jdm["working_dir"] as String
 
         for (s in script){
-            val r = executor.runShellCommand(s.command_arg_seq, wd)
-            logger.info(r.toString())
+            executor.commitShellCommand(s.command_arg_seq, wd)
         }
+    }
+
+    override fun interrupt() {
+        executor.stop()
     }
 }
--- src/main/kotlin/me/ckho/scriptscompose/service/impl/ScriptExecutorService.kt
+++ src/main/kotlin/me/ckho/scriptscompose/service/impl/ScriptExecutorService.kt
@@ -1,48 +1,131 @@
 package me.ckho.scriptscompose.service.impl
 
-import me.ckho.scriptscompose.domain.dataclasses.ShellReturn
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.io.BufferedReader
+import java.io.File
 import java.io.InputStreamReader
+import java.util.*
 import kotlin.concurrent.thread
 
+
 @Service
-class ScriptExecutorService{
-    fun runShellCommand(commands: List<String>, working_dir: String): ShellReturn {
-        val rt = Runtime.getRuntime()
-        val proc: Process = rt.exec(commands.toTypedArray())
+class ScriptExecutorService {
+    var logger: Logger = LoggerFactory.getLogger(ScriptExecutorService::class.java)
+    var commands: MutableList<Array<String>> = mutableListOf()
+    var workingDirs: MutableList<String> = mutableListOf()
+    var stopThreads = false
+    var taskRunning = false
+    var stdoutAccumulate: String = ""
+    var stderrAccumulate: String = ""
+    lateinit var proc: Process
+    var uid = "Script Executor Service -> ${UUID.randomUUID()}"
 
-        var stdoutAccumulate: String = ""
-        var stderrAccumulate: String = ""
-
-
-        thread {
-            val stdInput = BufferedReader(InputStreamReader(proc.inputStream))
-            var si: String?
-            while (stdInput.readLine().also {
-                    si = it
-                    if (it != null) {
-                        stdoutAccumulate += "$it\n"
-                    }
-                } != null) {
-                println(si)
-            }
-        }
-
-        thread {
-            val stdError = BufferedReader(InputStreamReader(proc.errorStream))
-            var se: String?
-            while (stdError.readLine().also {
-                    se = it
-                    if (it != null) {
-                        stderrAccumulate += "$it\n"
-                    }
-                } != null) {
-                println(se)
-            }
-        }
-
-        proc.waitFor()
-        return ShellReturn(stdoutAccumulate, stderrAccumulate)
+    fun commitShellCommand(command: List<String>, workingDir: String) {
+        commands.add(command.toTypedArray())
+        workingDirs.add(workingDir)
     }
+
+    fun runShellCommand(command: Array<String>, workingDir: String) {
+        println("Run ")
+        val rt = Runtime.getRuntime()
+        taskRunning = true
+        proc = rt.exec(command, null, File(workingDir))
+        proc.waitFor()
+        taskRunning = false
+    }
+
+    fun start() {
+        thread {
+            while (!stopThreads) {
+                if (taskRunning) {
+                    try {
+                        val stdInput = BufferedReader(InputStreamReader(proc.inputStream))
+                        var si: String?
+                        while (stdInput.readLine().also {
+                                si = it
+                                if (it != null) {
+                                    stdoutAccumulate += "$it\n"
+                                }
+                            } != null) {
+                            logger.info(si)
+                        }
+                    } catch (e: Exception) {
+                    }
+                }
+                Thread.sleep(500)
+            }
+        }
+
+        thread {
+            while (!stopThreads) {
+                if (taskRunning) {
+                    try {
+                        val stdError = BufferedReader(InputStreamReader(proc.errorStream))
+                        var se: String?
+                        while (stdError.readLine().also {
+                                se = it
+                                if (it != null) {
+                                    stderrAccumulate += "$it\n"
+                                }
+                            } != null) {
+                            logger.info(se)
+                        }
+                    } catch (e: Exception) {
+                    }
+                }
+                Thread.sleep(500)
+            }
+        }
+
+        thread {
+            while (!stopThreads) {
+                if (commands.size > 0) {
+                    val c = commands[0]
+                    val wd = workingDirs[0]
+                    runShellCommand(c, wd)
+                    commands.removeAt(0)
+                    workingDirs.removeAt(0)
+                }
+                Thread.sleep(500)
+            }
+        }
+    }
+
+    fun stop() {
+        proc.destroy()
+        stopThreads = true
+    }
+
 }
+
+//fun main() {
+//    val s = ScriptExecutorService()
+//    val input = Scanner(System.`in`)
+//    s.start()
+//    while (true) {
+//        try {
+//            print("REPL# ")
+//            val i = input.nextLine()
+//            when {
+//                i.startsWith("/commit") -> {
+//                    val file = i.split(" ")[1]
+//                    s.commitShellCommand(listOf("python", "-u", "sycm_compose.py", "-c $file"), ".")
+//                    println("A")
+//                }
+//                i.startsWith("/stop") -> {
+//                    s.stop()
+//                    break
+//                }
+//                i.startsWith("/commands") -> {
+//                    println(s.commands)
+//                }
+//                i.startsWith("/pwd") -> {
+//                    println("Working Directory = " + System.getProperty("user.dir"))
+//                }
+//            }
+//        } catch (e: Exception) {
+//        }
+//    }
+//}
